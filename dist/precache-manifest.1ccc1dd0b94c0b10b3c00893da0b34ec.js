ddbf4ad2472d01fd50a5d00c6728cf154eb7ac88
DemixArtix <demixarti@gmail.com>
2021-07-02 18:25:26 +0300

    try with dist

---
+++ dist/precache-manifest.1ccc1dd0b94c0b10b3c00893da0b34ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9890eb633bdaf7685df6",
+    "url": "/css/app.c49aa19a.css"
+  },
+  {
+    "revision": "b6509a6a55541c2de4c683a4f3fec93a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9890eb633bdaf7685df6",
+    "url": "/js/app.b8d7adef.js"
+  },
+  {
+    "revision": "c11bf52ae13a1b7a8286",
+    "url": "/js/chunk-vendors.f9138229.js"
+  },
+  {
+    "revision": "f7964dc3bbef0a53445434d1f4ba43de",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
